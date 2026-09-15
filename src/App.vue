06271e9f92d2dff9b73cd12efda4313543d70839
fanni108 <fannidakini@gmail.com>
2026-09-15 20:42:15 +0200

    added styling and magic button

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,12 @@ import Counter from './components/Counter.vue'
 </script>
 
 <template>
+  <main class = "page">
+    <section class = "content">
+      <p class="eyebrow"> </p>
+    <h1>Magic Counter</h1>
+    <p class="description">NextGenWeb Übung 1A</p>
   <Counter />
+    </section>
+  </main>
 </template>
--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -3,16 +3,35 @@
 
   const count = ref < number > (0)
   const doubled = computed(() => count.value * 2)
+  const magic = computed(() => {
+    if (count.value % 2 === 0) {
+      return 'even'
+    } else {
+      return 'odd'
+    }
+  })
 
 </script>
 
 <template>
-  <span>Count ist: {{ count }}</span>
-  <button class="counter" @click="count++">
-    +
-  </button>
-  <button class="counter" @click="count--">
+  <div class="counter-container">
+    <div class="counter-display">
+      <span class="counter-value">{{ count }}</span>
+    </div> 
+  <button class="ButtonCounter" @click="count--">
     -
   </button>
+  <button class="ButtonCounter" @click="count++">
+    +
+  </button>
+  <div class="counter-magic">
+  <span>Die Zahl ist: {{ magic }}</span>
+  </div>
+  </div>
+  <div class="counter-container">
+  <div class="counter-doubled">
   <span>Das doppelte ist: {{ doubled }}</span>
+  </div>
+  </div>
+
 </template>
