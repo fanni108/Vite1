1f5d30bcbaad6bc1aab492e7393fdd59e27da8e6
fanni108 <fannidakini@gmail.com>
2026-09-15 19:57:32 +0200

    first commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+  import { ref, computed } from 'vue'
+
+  const count = ref < number > (0)
+  const doubled = computed(() => count.value * 2)
+
+</script>
+
+<template>
+  <span>Count ist: {{ count }}</span>
+  <button class="counter" @click="count++">
+    +
+  </button>
+  <button class="counter" @click="count--">
+    -
+  </button>
+  <span>Das doppelte ist: {{ doubled }}</span>
+</template>
